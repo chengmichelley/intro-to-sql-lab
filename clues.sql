282305d319c6e8537a7b32a23b357a6961f82ff9
Michelle Cheng <cheng.michelley@gmail.com>
2026-06-02 20:09:06 -0400

    Clue 2

--- clues.sql
+++ clues.sql
@@ -5,8 +5,7 @@ SELECT name, code, population FROM countries WHERE region = 'Southern Europe' OR
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
--- Write SQL query here
-
+SELECT language FROM countrylanguages WHERE countrycode = 'VAT';
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
