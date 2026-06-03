87331c05223b32c0b3adc88be8b47de07725f2db
Michelle Cheng <cheng.michelley@gmail.com>
2026-06-02 20:42:24 -0400

    Clue 7

--- clues.sql
+++ clues.sql
@@ -59,3 +59,7 @@ WHERE code = 'BRA');
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+
+SELECT name, countrycode, population
+FROM cities
+WHERE population = 91084;
